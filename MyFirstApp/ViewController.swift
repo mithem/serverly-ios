17f1aa4e3322f08df50a1abe277a02d2de7a66c7
mithem <mithem@github.com>
2020-06-11 12:27:43 +0200

    Initial Commit

---
+++ MyFirstApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MyFirstApp
+//
+//  Created by Miguel Themann on 11.06.20.
+//  Copyright © 2020 Miguel Themann. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
